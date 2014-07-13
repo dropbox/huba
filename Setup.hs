c90dd76310477cc5a8c7cb0689d0406cab9d0137
David Fisher <ddfisher@dropbox.com>
2014-07-13 03:07:17 -0700

    Automatically regenerate thrift files on every cabal build
    Unfortunately, this slows down building considerably, because the files
    get recomiplied every time.  The actual thrift generation is extremely
    fast, though.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,6 @@
 import Distribution.Simple
-main = defaultMain
+import System.Process (callCommand)
+
+main = defaultMainWithHooks simpleUserHooks { preBuild = tackOn runThrift (preBuild simpleUserHooks) }
+  where tackOn f hook args bf = f >> hook args bf
+        runThrift = callCommand "command -v thrift > /dev/null && thrift -r --gen hs huba.thrift"
