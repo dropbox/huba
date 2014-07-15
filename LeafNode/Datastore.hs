a112ef3f4126399def10067d0e7dc519a4795504
David Fisher <ddfisher@dropbox.com>
2014-07-14 20:01:34 -0700

    Update thrift interface and other WIP

--- LeafNode/Datastore.hs
+++ LeafNode/Datastore.hs
@@ -8,3 +8,24 @@ type LeafStore = [LogMessage]
 
 ingestBatch :: LeafStore -> LogBatch -> LeafStore
 ingestBatch = foldl' (flip insert)
+
+
+
+-------------------------
+
+-- type ConName = String
+
+-- data AggFn = Constant | Sum | Count
+-- data Aggregator = Aggregator ColName AggFn
+
+
+-- project :: Row -> Aggregator -> Aggregate
+-- project row (Aggregator colName Sum) = Aggregate $ getCol colName row
+-- project row (Aggregator colName Count) = Aggregate $ 1
+-- project row (Aggregator colName Constant) = Aggregate $ getCol colName row
+
+
+-- aggAppend :: AggFn -> Aggregate -> Aggregate -> Aggregate
+-- aggAppend Sum (Aggregate x) (Aggregate y) = Aggregate (x + y)
+-- aggAppend Count (Aggregate x) (Aggregate y) = Aggregate (x + y)
+-- aggAppend Constant a1 _ = a1
