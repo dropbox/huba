4900c3d90e1729bd16425c8f3d09379def1a98c6
Tom McLaughlin <thomasjm@stanford.edu>
2014-07-16 15:10:17 -0700

    IngestorTest accepts batch size now

--- test/IngestorTest.hs
+++ test/IngestorTest.hs
@@ -16,56 +16,52 @@ import Network
 
 import qualified Huba_Types as T
 
-import Data.Sequence
-import Data.Text.Lazy
+import qualified Data.Sequence as S
+import qualified Data.Text.Lazy as L
 
 import qualified Data.Random as R
 import qualified Data.Random.Extras as RE
 import qualified Data.HashMap.Lazy as Map
 import qualified Data.Random.Source.DevRandom as R
 
--- sendLogMessages :: GHC.IO.Handle.Types.Handle -> [LogMessage] -> IO ()
--- sendLogMessages transport msgs = do
---   let binProto = BinaryProtocol transport
---   let protocols = (binProto, binProto)
+import Data.Foldable (toList)
 
---   resp <- Client.log protocols $ map toThrift msgs
+import Control.Monad (replicateM_, replicateM)
 
---   return
+import Data.Vector as V (fromList)
 
 main :: IO ()
 main = do
-
-  ports <- getArgs
+  args <- getArgs
+  let [numBatches, batchSize] = map (fromIntegral . read) args
 
   transport  <- hOpen ("localhost" :: String, PortNumber 8000)
   let binProto = BinaryProtocol transport
   let protocols = (binProto, binProto)
 
-  msg <- R.runRVar genRandomLogMessage R.DevURandom
-
-  resp <- Client.log protocols [toThrift msg]
-  putStrLn "Sent log message!"
-  print resp
+  replicateM_ numBatches $ do
+         -- Get batchSize random messages
+         msgs <- replicateM batchSize $ R.runRVar genRandomLogMessage R.DevURandom
+         resp <- Client.log protocols $ V.fromList $ map toThrift (toList msgs)
+         putStrLn $ "Got response: " ++ show resp
 
   -- Control.Exception.catch (printf "bad log message" =<< Client.log protocols 42.2)
   --       (\e -> printf "InvalidOperation %s\n" (show (e :: InvalidLogMessageException)))
 
-  -- Close!
   tClose transport
 
 
 tableChoices = ["tableA", "tableB", "tableC"]
 
-
-
--- TODO: make this a bit more interesting
 genRandomLogMessage = do
   ts <- R.uniform 0 100000
   table <- RE.choice tableChoices
 
-  string1 <- RE.sample 10 "abcdefghijklmnopqrstuvwxyz"
+  string1 <- RE.sample 10 ['a'..'z']
   int1 <- R.uniform 0 100
 
-  return $ LogMessage ts table $ Map.fromList [("string1", StringValue $ pack string1),
+  -- numStringsInVector <- R.uniform 0 10
+  -- TODO: make a vector with numStringsInVector random strings and add it to the message with some probability
+  -- TODO: make there be a chance of adding a random set of strings
+  return $ LogMessage ts table $ Map.fromList [("string1", StringValue $ L.pack string1),
                                                ("int1", IntValue int1)]
