a9f0a828fbd380655212116576be75591d3a9f21
David Fisher <ddfisher@dropbox.com>
2014-07-15 22:02:40 -0700

    Fix ingestor

--- src/Ingestor/Server.hs
+++ src/Ingestor/Server.hs
@@ -4,16 +4,26 @@ module Ingestor.Server (newIngestorHandler) where
 
 import Shared.Thrift.Types
 import Shared.Thrift.Interface
-import Data.Vector (Vector())
+import Shared.Thrift.ClientInterface
+
+import Shared.Config (ServerList, getLeafNodes)
+import System.Random (randomRIO)
+
+import Control.Applicative ((<$>))
+import Data.Maybe (fromJust)
 
 
-data IngestorHandler = IngestorHandler
+data IngestorHandler = IngestorHandler ServerList -- leaf nodes
 
 newIngestorHandler :: IO IngestorHandler
-newIngestorHandler = return IngestorHandler
+newIngestorHandler = IngestorHandler <$> getLeafNodes
 
+-- TODO: choose based on memory constraints
 instance IngestorService IngestorHandler where
-  logIngest :: IngestorHandler -> Vector LogMessage -> IO LogResponse
-  logIngest _ messages = do
-    putStrLn $ "Got messages: " ++ show messages
-    return $ LogResponse 0 "OK"
+  logIngest :: IngestorHandler -> LogBatch -> IO LogResponse
+  logIngest (IngestorHandler leaves) messages = do
+    leaf <- pick leaves
+    fromJust <$> sendLeafLog leaf messages -- XXX fromJust
+ 
+pick :: [a] -> IO a
+pick xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
