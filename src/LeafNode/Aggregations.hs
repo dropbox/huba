643da3c6e4bfd04cd12e83ca2d4f5bb32aeca2f2
Tom McLaughlin <tomm@dropbox.com>
2014-07-16 15:35:29 -0700

    Added other agg functions + tests

--- src/LeafNode/Aggregations.hs
+++ src/LeafNode/Aggregations.hs
@@ -17,7 +17,7 @@ initialConstant = RNull
 
 aggCount :: FoldFunction
 aggCount v Nothing = v
-aggCount (RIntValue i1) (Just (IntValue i2)) = RIntValue (i1 + i2)
+aggCount (RIntValue i1) (Just _) = RIntValue (i1 + 1)
 initialCount = RIntValue 0
 
 aggSum :: FoldFunction
@@ -25,11 +25,23 @@ aggSum v Nothing = v
 aggSum (RIntValue i1) (Just (IntValue i2)) = RIntValue (i1 + i2)
 initialSum = RIntValue 0
 
+aggMin :: FoldFunction
+aggMin v Nothing = v
+aggMin RNull (Just (IntValue i)) = RIntValue i
+aggMin (RIntValue i1) (Just (IntValue i2)) = RIntValue $ min i1 i2
+initialMin = RNull
+
+aggMax :: FoldFunction
+aggMax v Nothing = v
+aggMax RNull (Just (IntValue i)) = RIntValue i
+aggMax (RIntValue i1) (Just (IntValue i2)) = RIntValue $ max i1 i2
+initialMax = RNull
+
 aggFunctionAndInitialValue :: AggregationFunction -> (FoldFunction, ResponseValue)
 aggFunctionAndInitialValue CONSTANT = (aggConstant, initialConstant)
 aggFunctionAndInitialValue COUNT = (aggCount, initialCount)
-aggFunctionAndInitialValue MIN = undefined
-aggFunctionAndInitialValue MAX = undefined
+aggFunctionAndInitialValue MIN = (aggMin, initialMin)
+aggFunctionAndInitialValue MAX = (aggMax, initialMax)
 aggFunctionAndInitialValue SUM = (aggSum, initialSum)
 aggFunctionAndInitialValue AVERAGE =  undefined
 aggFunctionAndInitialValue SUM_PER_MINUTE =  undefined
--- test/LeafNode/DatastoreSpec.hs
+++ test/LeafNode/DatastoreSpec.hs
@@ -33,7 +33,7 @@ spec = do
     it "returns all messages to the right" $
        testQueryResponse allMessagesStore
                          (basicTimeRangeQuery 15 100)
-                         (successfulQueryResponse [Row [RStringValue "s2"], Row [RStringValue "s3"]])
+                         (successfulQueryResponse [Row [RStringValue "s2"], Row [RStringValue "s3"], Row [RStringValue "s3"]])
 
   describe "Simple projection queries" $
     it "should project out columns" $
@@ -41,14 +41,29 @@ spec = do
                          (Query [ColumnExpression "int1" CONSTANT, ColumnExpression "string1" CONSTANT] "some-table" 0 100 Nothing Nothing Nothing 100)
                          (successfulQueryResponse [Row [RIntValue 42, RStringValue "s1"],
                                                   Row [RIntValue 100, RStringValue "s2"],
-                                                  Row [RIntValue 0, RStringValue "s3"]])
+                                                  Row [RIntValue 0, RStringValue "s3"],
+                                                  Row [RIntValue 15, RStringValue "s3"]])
 
-  describe "Aggregations" $
+  describe "Aggregations" $ do
     it "can do a count aggregation" $
        testQueryResponse allMessagesStore
                          (Query [ColumnExpression "int1" COUNT, ColumnExpression "string1" CONSTANT] "some-table" 0 100 Nothing Nothing Nothing 100)
-                         (successfulQueryResponse [Row [RIntValue 142, RStringValue "s1"]])
+                         (successfulQueryResponse [Row [RIntValue 4, RStringValue "s1"]])
 
+    it "can do a min aggregation" $
+       testQueryResponse allMessagesStore
+                         (Query [ColumnExpression "int1" MIN, ColumnExpression "string1" CONSTANT] "some-table" 0 100 Nothing Nothing Nothing 100)
+                         (successfulQueryResponse [Row [RIntValue 0, RStringValue "s1"]])
+
+    it "can do a max aggregation" $
+       testQueryResponse allMessagesStore
+                         (Query [ColumnExpression "int1" MAX, ColumnExpression "string1" CONSTANT] "some-table" 0 100 Nothing Nothing Nothing 100)
+                         (successfulQueryResponse [Row [RIntValue 100, RStringValue "s1"]])
+
+    it "can do a sum aggregation" $
+       testQueryResponse allMessagesStore
+                         (Query [ColumnExpression "int1" SUM, ColumnExpression "string1" CONSTANT] "some-table" 0 100 Nothing Nothing Nothing 100)
+                         (successfulQueryResponse [Row [RIntValue 157, RStringValue "s1"]])
 
 -- Helper functions --
 
@@ -65,5 +80,6 @@ successfulQueryResponse rows = QueryResponse 0 Nothing (Just rows)
 lm1 = LogMessage 10 "some-table" [("string1", StringValue "s1"), ("int1", IntValue 42), ("vector1", StringVector ["a", "b", "c"])]
 lm2 = LogMessage 20 "some-table" [("string1", StringValue "s2"), ("int1", IntValue 100), ("vector2", StringVector ["b", "c"])]
 lm3 = LogMessage 30 "some-table" [("string1", StringValue "s3"), ("int1", IntValue 0), ("vector3", StringVector ["x", "y", "z"])]
+lm4 = LogMessage 40 "some-table" [("string1", StringValue "s3"), ("int1", IntValue 15), ("vector3", StringVector ["p", "q", "r"])]
 
-allMessagesStore = ingestBatch [] [lm1, lm2, lm3]
+allMessagesStore = ingestBatch [] [lm1, lm2, lm3, lm4]
