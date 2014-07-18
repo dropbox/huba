83ac016a7f0b87189f1c93a12d2bb4d63186eaeb
David Fisher <ddf1991@gmail.com>
2014-07-17 18:39:34 -0700

    Create test database implementation

--- src/Shared/Thrift/Types/Internal.hs
+++ src/Shared/Thrift/Types/Internal.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, DeriveGeneric #-}
 
 module Shared.Thrift.Types.Internal where
 
@@ -14,6 +14,9 @@ import Data.Ord (comparing)
 
 import Control.Lens.TH
 
+import Data.Hashable
+import GHC.Generics
+
 type ColumnName = T.ColumnName
 type ServerID = T.ServerID
 type Timestamp = Int64
@@ -23,12 +26,12 @@ data ColumnValue = StringValue Text
                  | IntValue Timestamp
                  | StringSet (Set.HashSet Text)
                  | StringVector (Vector Text)
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
 
 data LogMessage = LogMessage { _lmTimestamp :: Timestamp
                              , _lmTable :: Text
                              , _lmColumns :: Map.HashMap ColumnName ColumnValue
-                             } deriving (Show, Eq)
+                             } deriving (Show, Eq, Generic)
 
 instance Ord LogMessage where
     compare = comparing _lmTimestamp `orComparing` _lmTable
@@ -40,11 +43,11 @@ type LogBatch = Vector LogMessage
 
 data LogResponse = LogResponse { _lrCode :: Int32
                                , _lrMessage :: Maybe Text
-                               } deriving Show
+                               } deriving (Show, Eq, Generic)
 
 data PingResponse = PingResponse { _prCode :: Int32
                                  , _prMessage :: Maybe Text
-                                 } deriving Show
+                                 } deriving (Show, Eq, Generic)
 
 type AggregationFunction = T.AggregationFunction
 
@@ -77,7 +80,9 @@ data ResponseValue = RDoubleValue Double
                    | RStringSet (Set.HashSet Text)
                    | RStringVector (Vector Text)
                    | RNull
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
+
+instance Hashable ResponseValue
 
 -- data Row = Row { _rValues :: Vector ResponseValue }
 --   deriving (Show, Eq)
---
+++ src/Testing/TestDatabase.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE ViewPatterns #-}
+module Testing.TestDatabase where
+
+import Shared.Thrift.Types
+import Shared.Comparison (makeCondition)
+import Control.Lens
+import Data.List (sortBy)
+import Data.Ord (comparing)
+import qualified Data.Vector as V
+import qualified Data.HashMap.Lazy as M
+
+testQuery :: [LogMessage] -> Query -> [Row]
+testQuery ms (Query cols table startTime endTime conditions group order limit)
+  = ms & filterTable table
+       & filterTime startTime endTime
+       & filterConditions conditions
+       & pluckColumns cols
+       & groupBy group
+       & orderBy order
+       & take limit
+  where filterTable t = filter (^. lmTable . to (== t))
+        filterTime start end = filter (^. lmTimestamp . to (\t -> t >= start && t <= end))
+        filterConditions Nothing = id
+        filterConditions (Just cs) = let conditionFns = V.map makeCondition cs in
+                                       filter (\msg -> V.all ($ msg) conditionFns)
+        pluckColumns cs = map (project cs)
+        groupBy Nothing   rs = rs
+        groupBy (Just gs) rs = let Just is = V.mapM (\g -> V.elemIndex (ColumnExpression g CONSTANT) cols) gs
+                                   getGroup rvs = V.map (rvs V.!) is in
+                               rs & map (\r@(Row rvs) -> (getGroup rvs, r))
+                                  & M.fromListWith (combine cols)
+                                  & M.elems
+        orderBy Nothing = id
+        orderBy (Just o) = sortBy (comparing ((V.! o) . view rValues))
+
+
+project :: V.Vector ColumnExpression -> LogMessage -> Row
+project cols LogMessage { _lmColumns = colMap }
+  = Row $ V.map findAndProject cols
+  where findAndProject (ColumnExpression name agg) = case name `M.lookup` colMap of
+                                                       Nothing -> RNull
+                                                       Just colValue -> projectCol agg colValue
+
+projectCol :: AggregationFunction -> ColumnValue -> ResponseValue
+projectCol CONSTANT (StringValue v)        = RStringValue v
+projectCol CONSTANT (IntValue v)           = RIntValue v
+projectCol CONSTANT (StringSet v)          = RStringSet v
+projectCol CONSTANT (StringVector v)       = RStringVector v
+
+projectCol COUNT _                         = RIntValue 1
+
+projectCol MIN (IntValue v)                = RIntValue v
+projectCol MIN _                           = RNull
+
+projectCol MAX (IntValue v)                = RIntValue v
+projectCol MAX _                           = RNull
+
+projectCol SUM (IntValue v)                = RIntValue v
+projectCol SUM _                           = RNull
+
+projectCol AVERAGE (IntValue _)            = error "Average not supported"
+projectCol AVERAGE _                       = RNull
+
+projectCol SUM_PER_MINUTE (IntValue _)     = error "Sum per minute not supported"
+projectCol SUM_PER_MINUTE _                = RNull
+
+projectCol HISTOGRAM _                     = error "Histogram not supported"
+
+
+
+combine :: V.Vector ColumnExpression -> Row -> Row -> Row
+combine (V.map (view ceAggregationFunction) -> aggFns) (Row vs1) (Row vs2)
+  = Row $ V.zipWith3 combineCol aggFns vs1 vs2
+
+
+combineCol :: AggregationFunction -> ResponseValue -> ResponseValue -> ResponseValue
+combineCol CONSTANT v _                        = v
+
+combineCol COUNT (RIntValue v1) (RIntValue v2) = RIntValue $ v1 + v2
+combineCol COUNT _              (RIntValue v2) = RIntValue $ v2
+combineCol COUNT (RIntValue v1) _              = RIntValue $ v1
+combineCol COUNT _              _              = RNull
+
+combineCol MIN (RIntValue v1) (RIntValue v2)   = RIntValue $ min v1 v2
+combineCol MIN _              (RIntValue v2)   = RIntValue $ v2
+combineCol MIN (RIntValue v1) _                = RIntValue $ v1
+combineCol MIN _              _                = RNull
+
+combineCol MAX (RIntValue v1) (RIntValue v2)   = RIntValue $ max v1 v2
+combineCol MAX _              (RIntValue v2)   = RIntValue $ v2
+combineCol MAX (RIntValue v1) _                = RIntValue $ v1
+combineCol MAX _              _                = RNull
+
+combineCol SUM (RIntValue v1) (RIntValue v2)   = RIntValue $ v1 + v2
+combineCol SUM _              (RIntValue v2)   = RIntValue $ v2
+combineCol SUM (RIntValue v1) _                = RIntValue $ v1
+combineCol SUM _              _                = RNull
+
+combineCol AVERAGE _ _                         = error "Average not supported"
+
+combineCol SUM_PER_MINUTE _ _                  = error "Sum per minute not supported"
+
+combineCol HISTOGRAM _ _                       = error "Histogram not supported"
